164fcaca9e50ca604b764e299e9221afebecc771
tuankiet1803 <tuankiet18032004@gmail.com>
2024-10-29 15:01:20 +0700

    Physical and Enemy
    hoan thanh tuong tac vat li va tao npc va enemy

---
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 movedir;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movedir * (moveSpeed * Time.fixedDeltaTime));
+    }
+    public void MoveTo(Vector2 targetPosition)
+    {
+        movedir = targetPosition;
+    }
+}
